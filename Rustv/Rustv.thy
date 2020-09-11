88a32b5f97e5960e1443b7dae0e8942109e9f8ac
pandaman64 <kointosudesuyo@infoseek.jp>
2020-09-11 21:47:32 +0900

    track tag uniqueness in a stack

--- Rustv/Rustv.thy
+++ Rustv/Rustv.thy
@@ -25,32 +25,42 @@ record globals_ram =
   tags :: "(ref_kind * tag set) stack list"
   issued_tags :: "tag list"
 
+fun collect_tags_stack :: "(ref_kind * tag set) stack \<Rightarrow> tag set" where
+  "collect_tags_stack stack = \<Union>(set (map snd stack))"
+lemma collect_tags_stack_spec:
+  "t \<in> collect_tags_stack stack \<longleftrightarrow> (\<exists>entry \<in> set stack. t \<in> snd entry)"
+proof (induction stack)
+qed auto
+
 inductive wf_reborrow :: "(ref_kind * tag set) stack \<Rightarrow> bool" where
   (* An allocation root is a Unique reference (Box) *)
   BorrowRoot: "wf_reborrow [(Unique, {t})]" |
 
   (* Reborrowing from a Unique reference *)
   ReborrowUniqueUnique:
-    "wf_reborrow ((Unique, {t}) # tail) \<Longrightarrow> wf_reborrow ((Unique, {t'}) # (Unique, {t}) # tail)" |
+    "\<lbrakk>wf_reborrow ((Unique, {t}) # tail); t' \<notin> insert t (collect_tags_stack tail)\<rbrakk>
+ \<Longrightarrow> wf_reborrow ((Unique, {t'}) # (Unique, {t}) # tail)" |
   ReborrowUniqueSRW:
-    "wf_reborrow ((Unique, {t}) # tail) \<Longrightarrow> wf_reborrow ((SharedReadWrite, ts) # (Unique, {t}) # tail)" |
+    "\<lbrakk>wf_reborrow ((Unique, {t}) # tail); t' \<notin> insert t (collect_tags_stack tail)\<rbrakk>
+ \<Longrightarrow> wf_reborrow ((SharedReadWrite, {t'}) # (Unique, {t}) # tail)" |
   ReborrowUniqueSRO:
-    "wf_reborrow ((Unique, {t}) # tail) \<Longrightarrow> wf_reborrow ((SharedReadOnly, ts) # (Unique, {t}) # tail)" |
+    "\<lbrakk>wf_reborrow ((Unique, {t}) # tail); t' \<notin> insert t (collect_tags_stack tail)\<rbrakk>
+ \<Longrightarrow> wf_reborrow ((SharedReadOnly, {t'}) # (Unique, {t}) # tail)" |
 
   (* Reborrowing from a SharedReadWrite *)
   ReborrowSRWUnique:
-    "wf_reborrow ((SharedReadWrite, ts) # tail)
+    "\<lbrakk>wf_reborrow ((SharedReadWrite, ts) # tail); t' \<notin> ts \<union> collect_tags_stack tail\<rbrakk>
  \<Longrightarrow> wf_reborrow ((Unique, {t'}) # (SharedReadWrite, ts) # tail)" |
   ReborrowSRWSRW:
-    "wf_reborrow ((SharedReadWrite, ts) # tail)
+    "\<lbrakk>wf_reborrow ((SharedReadWrite, ts) # tail); t \<notin> ts \<union> collect_tags_stack tail\<rbrakk>
  \<Longrightarrow> wf_reborrow ((SharedReadWrite, insert t ts) # tail)" |
   ReborrowSRWSRO:
-    "wf_reborrow ((SharedReadWrite, ts) # tail)
- \<Longrightarrow> wf_reborrow ((SharedReadOnly, ts') # (SharedReadWrite, ts) # tail)" |
+    "\<lbrakk>wf_reborrow ((SharedReadWrite, ts) # tail); t' \<notin> ts \<union> collect_tags_stack tail\<rbrakk>
+ \<Longrightarrow> wf_reborrow ((SharedReadOnly, {t'}) # (SharedReadWrite, ts) # tail)" |
 
   (* Reborrowing from a SharedReadOnly gives only a shared reference *)
   ReborrowSROSRO:
-    "wf_reborrow ((SharedReadOnly, ts) # tail)
+    "\<lbrakk>wf_reborrow ((SharedReadOnly, ts) # tail); t \<notin> ts \<union> collect_tags_stack tail\<rbrakk>
  \<Longrightarrow> wf_reborrow ((SharedReadOnly, insert t ts) # tail)"
 
 inductive_cases wf_reborrow_elims: "wf_reborrow stack"
@@ -96,6 +106,248 @@ lemma wf_reborrow_pop':
 using assms proof (induction "(top # rest)" arbitrary: top rule: wf_reborrow.induct)
 qed auto
 
+lemma shared_read_only_top:
+  assumes
+    "wf_reborrow stack"
+    "\<exists>entry \<in> set stack. fst entry = SharedReadOnly"
+  shows "fst (hd stack) = SharedReadOnly \<and> \<not>(\<exists>entry \<in> set (tl stack). fst entry = SharedReadOnly)"
+using assms proof (induction stack rule: wf_reborrow.induct)
+qed auto
+
+lemma wf_reborrow_top_tag_notin:
+  assumes
+    "wf_reborrow stack"
+    "target \<in> snd (hd stack)"
+  shows "target \<notin> collect_tags_stack (tl stack)"
+using assms proof (induction stack rule: wf_reborrow.induct)
+qed auto
+
+lemma wf_reborrow_tag_position_inj_zero:
+  assumes
+    "wf_reborrow stack"
+    "i < length stack"
+    "target \<in> snd (stack ! i)"
+    "target \<in> snd (stack ! 0)"
+  shows "i = 0"
+proof (cases i)
+  case 0
+  then show ?thesis by simp
+next
+  case (Suc i')
+  have "target \<in> snd (hd stack)" using assms wf_reborrow_nonempty
+    by (simp add: hd_conv_nth)
+  moreover have "target \<in> collect_tags_stack (tl stack)"
+    using assms collect_tags_stack_spec nth_tl wf_reborrow_nonempty
+    by (metis Nitpick.size_list_simp(2) Suc Suc_less_SucD nth_mem)
+  ultimately show ?thesis using wf_reborrow_top_tag_notin assms(1) by blast
+qed
+
+(* repetitive, obvious proof *)
+lemma wf_reborrow_tag_position_inj:
+  assumes
+    "wf_reborrow stack"
+    "i < length stack"
+    "j < length stack"
+    "target \<in> snd (stack ! i)"
+    "target \<in> snd (stack ! j)"
+  shows "i = j"
+using assms proof (induction stack arbitrary: i j rule: wf_reborrow.induct)
+  case (BorrowRoot t)
+  then show ?case by simp
+next
+  case (ReborrowUniqueUnique t tail t')
+  show ?case
+  proof (cases i)
+    case 0
+    then show ?thesis
+      using ReborrowUniqueUnique wf_reborrow.ReborrowUniqueUnique
+        wf_reborrow_tag_position_inj_zero by blast
+  next
+    case (Suc i')
+    note Suci = Suc
+    then show ?thesis
+    proof (cases j)
+      case 0
+      then show ?thesis
+        using ReborrowUniqueUnique wf_reborrow.ReborrowUniqueUnique
+          wf_reborrow_tag_position_inj_zero by blast
+    next
+      case (Suc j')
+      then show ?thesis using Suc Suci ReborrowUniqueUnique by auto
+    qed
+  qed
+next
+  case (ReborrowUniqueSRW t tail t')
+  then show ?case
+  proof (cases i)
+    case 0
+    then show ?thesis
+      using ReborrowUniqueSRW wf_reborrow.ReborrowUniqueSRW
+        wf_reborrow_tag_position_inj_zero by blast
+  next
+    case (Suc i')
+    note Suci = Suc
+    then show ?thesis
+    proof (cases j)
+      case 0
+      then show ?thesis
+        using ReborrowUniqueSRW wf_reborrow.ReborrowUniqueSRW
+          wf_reborrow_tag_position_inj_zero by blast
+    next
+      case (Suc j')
+      then show ?thesis using Suc Suci ReborrowUniqueSRW by auto
+    qed
+  qed
+next
+  case (ReborrowUniqueSRO t tail t')
+  then show ?case
+  proof (cases i)
+    case 0
+    then show ?thesis
+      using ReborrowUniqueSRO wf_reborrow.ReborrowUniqueSRO
+        wf_reborrow_tag_position_inj_zero by blast
+  next
+    case (Suc i')
+    note Suci = Suc
+    then show ?thesis
+    proof (cases j)
+      case 0
+      then show ?thesis
+      using ReborrowUniqueSRO wf_reborrow.ReborrowUniqueSRO
+        wf_reborrow_tag_position_inj_zero by blast
+    next
+      case (Suc j')
+      then show ?thesis using Suc Suci ReborrowUniqueSRO by auto
+    qed
+  qed
+next
+  case (ReborrowSRWUnique ts tail t')
+  then show ?case
+  proof (cases i)
+    case 0
+    then show ?thesis
+      using ReborrowSRWUnique wf_reborrow.ReborrowSRWUnique
+        wf_reborrow_tag_position_inj_zero by blast
+  next
+    case (Suc i')
+    note Suci = Suc
+    then show ?thesis
+    proof (cases j)
+      case 0
+      then show ?thesis
+      using ReborrowSRWUnique wf_reborrow.ReborrowSRWUnique
+        wf_reborrow_tag_position_inj_zero by blast
+    next
+      case (Suc j')
+      then show ?thesis using Suc Suci ReborrowSRWUnique by auto
+    qed
+  qed
+next
+  case (ReborrowSRWSRW ts tail t)
+  then show ?case
+  proof (cases i)
+    case 0
+    then show ?thesis
+      using ReborrowSRWSRW wf_reborrow.ReborrowSRWSRW
+        wf_reborrow_tag_position_inj_zero by blast
+  next
+    case (Suc i')
+    note Suci = Suc
+    then show ?thesis
+    proof (cases j)
+      case 0
+      then show ?thesis
+      using ReborrowSRWSRW wf_reborrow.ReborrowSRWSRW
+        wf_reborrow_tag_position_inj_zero by blast
+    next
+      case (Suc j')
+      then show ?thesis using Suc Suci ReborrowSRWSRW
+        by (metis length_Cons nth_Cons_Suc)
+    qed
+  qed
+next
+  case (ReborrowSRWSRO ts tail t')
+  then show ?case
+  proof (cases i)
+    case 0
+    then show ?thesis
+      using ReborrowSRWSRO wf_reborrow.ReborrowSRWSRO
+        wf_reborrow_tag_position_inj_zero by blast
+  next
+    case (Suc i')
+    note Suci = Suc
+    then show ?thesis
+    proof (cases j)
+      case 0
+      then show ?thesis
+      using ReborrowSRWSRO wf_reborrow.ReborrowSRWSRO
+        wf_reborrow_tag_position_inj_zero by blast
+    next
+      case (Suc j')
+      then show ?thesis using Suc Suci ReborrowSRWSRO by auto
+    qed
+  qed
+next
+  case (ReborrowSROSRO ts tail t)
+  then show ?case
+  proof (cases i)
+    case 0
+    then show ?thesis
+      using ReborrowSROSRO wf_reborrow.ReborrowSROSRO
+        wf_reborrow_tag_position_inj_zero by blast
+  next
+    case (Suc i')
+    note Suci = Suc
+    then show ?thesis
+    proof (cases j)
+      case 0
+      then show ?thesis
+      using ReborrowSROSRO wf_reborrow.ReborrowSROSRO
+        wf_reborrow_tag_position_inj_zero by blast
+    next
+      case (Suc j')
+      then show ?thesis using Suc Suci ReborrowSROSRO
+        by (metis length_Cons nth_Cons_Suc)
+    qed
+  qed
+qed
+
+lemma wf_reborrow_tag_position_unique':
+  assumes
+    "wf_reborrow stack"
+    "target \<in> collect_tags_stack stack"
+  shows "\<exists>i. (i < length stack \<and> target \<in> snd (stack ! i)) \<and>
+  (\<forall>j. j < length stack \<and> target \<in> snd (stack ! j) \<longrightarrow> j = i)"
+proof -
+  have "\<exists>i < length stack. target \<in> snd (stack ! i)"
+    using assms collect_tags_stack_spec by (metis in_set_conv_nth)
+  then obtain i where
+    "i < length stack"
+    "target \<in> snd (stack ! i)"
+    by auto
+  moreover
+  note calc = calculation
+  have "\<forall>j. j < length stack \<and> target \<in> snd (stack ! j) \<longrightarrow> j = i"
+  proof
+    fix j
+    show "j < length stack \<and> target \<in> snd (stack ! j) \<longrightarrow> j = i"
+    proof
+      assume "j < length stack \<and> target \<in> snd (stack ! j)"
+      then show "j = i" using calc assms wf_reborrow_tag_position_inj by auto
+    qed
+  qed
+  ultimately show "\<exists>i. (i < length stack \<and> target \<in> snd (stack ! i)) \<and>
+    (\<forall>j. j < length stack \<and> target \<in> snd (stack ! j) \<longrightarrow> j = i)"
+    by blast
+qed
+
+lemma wf_reborrow_tag_position_unique:
+  assumes
+    "wf_reborrow stack"
+    "target \<in> collect_tags_stack stack"
+  shows "\<exists>!i. i < length stack \<and> target \<in> snd (stack ! i)"
+  using wf_reborrow_tag_position_unique' assms by blast
+
 fun stack_finite :: "(ref_kind * tag set) stack \<Rightarrow> bool" where
   "stack_finite [] = True" |
   "stack_finite (s # stack) = (finite (snd s) \<and> stack_finite stack)"
@@ -124,9 +376,6 @@ lemma wf_tags_update:
     "stack_finite stack"
   shows "wf_tags (ts[p := stack])"
   using assms set_update_subset_insert wf_tags_spec by fastforce
-
-fun collect_tags_stack :: "(ref_kind * tag set) stack \<Rightarrow> tag set" where
-  "collect_tags_stack stack = \<Union>(set (map snd stack))"
 
 fun collect_tags :: "(ref_kind * tag set) stack list \<Rightarrow> tag set" where
   "collect_tags [] = {}" |
@@ -204,15 +453,14 @@ fun wf_heap :: "'a globals_ram_scheme \<Rightarrow> bool" where
     \<and> wf_tags (tags s)
     \<and> collect_tags (tags s) \<subseteq> set (issued_tags s)"
 
-(* when we use a unique reference, invalidate all children *)
-fun invalidate_children :: "tagged_ref \<Rightarrow> 'a globals_ram_scheme \<Rightarrow> 'a globals_ram_scheme" where
-  "invalidate_children r s =
+fun pop_tags :: "tagged_ref \<Rightarrow> 'a globals_ram_scheme \<Rightarrow> 'a globals_ram_scheme" where
+  "pop_tags r s =
     (let p = the_ptr (pointer r);
          stack = tags s ! p;
          stack' = dropWhile (\<lambda>entry. (tag r) \<notin> snd entry) stack in
     s\<lparr> tags := (tags s)[p := stack'] \<rparr>)"
 
-fun_cases invalidate_childrenE: "invalidate_children r s = s'"
+fun_cases pop_tags_elims: "pop_tags r s = s'"
 
 lemma hd_dropWhile_stack:
   assumes
@@ -228,7 +476,6 @@ proof (induction stack)
   then show ?case using wf_reborrow_nonempty by simp
 next
   case (Cons top rest)
-
   show ?case
   proof (cases "P top")
     case True
@@ -238,34 +485,81 @@ next
     ultimately show ?thesis using Cons.IH by auto
   next
     case False
-    then have "dropWhile P (top # rest) = top # rest" by simp
     then show ?thesis using Cons.prems by simp
   qed
 qed
-(*
-lemma invalidate_wf_tags:
+
+lemma dropWhile_stack_finite:
+  "stack_finite stack \<Longrightarrow> stack_finite (dropWhile P stack)"
+proof (induction stack)
+  case Nil
+  then show ?case by simp
+next
+  case (Cons st stack)
+  then have *: "stack_finite (dropWhile P stack)" by simp
+  then show ?case
+  proof (cases "P st")
+    case True
+    then show ?thesis using * by simp
+  next
+    case False
+    then show ?thesis using Cons.prems by simp
+  qed
+qed
+
+lemma pop_tags_wf_tags:
   assumes
-    "invalidate_children r s = s'"
-    "\<exists>entry \<in> set ((tags s) ! the_ptr (pointer r)). (tag r) \<in> snd entry"
+    "pop_tags r s = s'"
+    "the_ptr (pointer r) < length (tags s)"
+    "(tags s') ! the_ptr (pointer r) \<noteq> []"
     "wf_tags (tags s)"
   shows "wf_tags (tags s')"
-  sorry
+  using assms(1) apply (rule pop_tags_elims, simp add: Let_def)
+  apply (rule wf_tags_update)
+  using assms(4) apply simp
+  using assms dropWhile_stack_reborrow wf_tags_spec apply simp
+  using assms dropWhile_stack_finite wf_tags_spec by simp
 
-lemma
+lemma hd_pop_tags:
   assumes
-    "invalidate_children r s = s'"
-    "wf_tags (tags s)"
+    "pop_tags r s = s'"
     "the_ptr (pointer r) < length (tags s)"
-    "tag r \<in> collect_tag_stack ((tags s') ! the_ptr (pointer r))"
+    "tag r \<in> collect_tags_stack ((tags s) ! the_ptr (pointer r))"
   shows "tag r \<in> snd (hd ((tags s') ! the_ptr (pointer r)))"
-  using assms(1) apply (rule invalidate_childrenE)
-  apply (simp add: Let_def)
-  by (simp add: Let_def set_dropWhileD) *)
+  using assms(1) apply (rule pop_tags_elims)
+  using assms hd_dropWhile_stack Let_def  by (auto simp add: Let_def)
 
-lemma invalidate_children_memory[intro]:
-  "invalidate_children r s = s' \<Longrightarrow> memory s' = memory s"
-  apply (erule invalidate_children.elims)
+lemma pop_tags_memory[intro]:
+  "pop_tags r s = s' \<Longrightarrow> memory s' = memory s"
+  apply (erule pop_tags.elims)
   by (simp add: Let_def)
+
+lemma pop_tags_tags_length:
+  assumes "pop_tags r s = s'"
+  shows "length (tags s) = length (tags s')"
+using assms proof (rule pop_tags_elims, auto simp add: Let_def)
+qed
+
+lemma pop_tags_result:
+  assumes
+    "pop_tags r s = s'"
+    "the_ptr (pointer r) < length (tags s)"
+    "p < length (tags s)"
+  shows "\<exists>start. (tags s) ! p =
+    (if p = the_ptr (pointer r) then
+      start @ (tags s') ! p
+    else
+       (tags s') ! p)"
+proof -
+  have "p \<noteq> the_ptr (pointer r) \<Longrightarrow> (tags s) ! p = (tags s') ! p"
+    using assms(1) apply (rule pop_tags_elims)
+    by (auto simp add: Let_def)
+  moreover have "p = the_ptr (pointer r) \<Longrightarrow> \<exists>start. (tags s) ! p = start @ (tags s') ! p"
+    using assms(1) apply (rule pop_tags_elims)
+    apply (auto simp add: Let_def)
+    by (metis assms(2) takeWhile_dropWhile_id nth_list_update_eq)
+  ultimately show ?thesis by simp
+qed
 
 fun memwrite
   :: "tagged_ref \<Rightarrow> val \<Rightarrow> 'a globals_ram_scheme \<Rightarrow> 'a globals_ram_scheme"
@@ -335,9 +629,41 @@ qed
 
 fun writable_stack :: "tag \<Rightarrow> (ref_kind * tag set) stack \<Rightarrow> bool" where
   "writable_stack t [] \<longleftrightarrow> False" |
-  "writable_stack t ((Unique, ts) # stack) \<longleftrightarrow> t \<in> ts \<or> writable_stack t stack" |
-  "writable_stack t ((SharedReadWrite, ts) # stack) \<longleftrightarrow> t \<in> ts \<or> writable_stack t stack" |
-  "writable_stack t ((SharedReadOnly, ts) # stack) \<longleftrightarrow> writable_stack t stack"
+  "writable_stack t ((k, ts) # stack) \<longleftrightarrow>
+    (if t \<in> ts then k = Unique \<or> k = SharedReadWrite else writable_stack t stack)"
+
+lemma writable_stack_in_collect_tags:
+  assumes "writable_stack t stack"
+  shows "t \<in> collect_tags_stack stack"
+using assms proof (induction stack)
+  case (Cons entry stack)
+  then show ?case using writable_stack.elims(2) by fastforce
+qed auto
+
+lemma writable_stack_the_index:
+  assumes
+    "wf_reborrow stack"
+    "writable_stack t stack"
+  shows "\<exists>!i. i < length stack \<and> t \<in> snd (stack ! i)"
+  using wf_reborrow_tag_position_unique writable_stack_in_collect_tags assms by simp
+
+
+
+lemma writable_stack_pop_tags_nonempty:
+  assumes
+    "pop_tags r s = s'"
+    "the_ptr (pointer r) < length (tags s)"
+    "writable_stack (tag r) ((tags s) ! the_ptr (pointer r))"
+  shows "(tags s') ! the_ptr (pointer r) \<noteq> []"
+  sorry
+
+lemma writable_stack_pop_tags_update:
+  assumes
+    "pop_tags r s = s'"
+    "the_ptr (pointer r) < length (tags s)"
+    "writable_stack (tag r) ((tags s) ! the_ptr (pointer r))"
+  shows "writable_stack (tag r) ((tags s') ! the_ptr (pointer r))"
+  sorry
 
 fun writable :: "tagged_ref \<Rightarrow> 'a globals_ram_scheme \<Rightarrow> bool" where
   "writable r s =
@@ -348,9 +674,9 @@ fun writable :: "tagged_ref \<Rightarrow> 'a globals_ram_scheme \<Rightarrow> bo
 lemma writable_update[simp]: "writable r (memwrite r' v s) = writable r s"
   by simp
 
-lemma writable_invalidated[intro]: "writable r s \<Longrightarrow> writable r (invalidate_children r s)"
+lemma writable_invalidated[intro]: "writable r s \<Longrightarrow> writable r (pop_tags r s)"
   apply (simp add: Let_def)
-  apply (auto simp add: nth_list_update_eq)
+  apply auto
   by (metis dropWhile_in list_update_beyond not_le nth_list_updateeq)
 
 fun new_pointer :: "'a globals_ram_scheme \<Rightarrow> pointer" where
